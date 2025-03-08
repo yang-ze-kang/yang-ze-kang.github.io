0d30ef8b8d96a2c60a1b8a683f89bc90264fe4ad
杨泽康 <yangzekang@YangMac.local>
2025-03-08 21:13:32 +0800

    250308

---
+++ _pages/includes/starts.md
@@ -0,0 +1,16 @@
+<table>
+<thead>
+  <tr>
+    <td>
+      <a href="https://github.com/anuraghazra/github-readme-stats">
+        <img align="center" src="https://github-readme-stats.vercel.app/api?username=yang-ze-kang&hide_border=true"/>
+      </a>
+    </td>
+    <td>
+      <a href="https://github.com/anuraghazra/convoychat">
+        <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=yang-ze-kang&layout=compact&hide_border=true" />
+      </a>
+    </td>
+  </tr>
+</thead>
+</table>
--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -3,8 +3,9 @@ if __name__ == '__main__':
     base_dir = '../_pages/includes/'
     _intro = open(f'{base_dir}/intro.md').read().strip()
     _homepage = open(f'{base_dir}/homepage.md').read().strip()
-    _pub = open(f'{base_dir}/pub_short.md').read().strip()
-    _news = open(f'{base_dir}/news.md').read().strip()
+    _pub = open(f'{base_dir}/pub.md').read().strip()
+    # _news = open(f'{base_dir}/news.md').read().strip()
+    _starts = open(f'{base_dir}/starts.md').read().strip()
     with open('README.md', 'w') as f:
         f.write(_header)
         f.write('\n\n')
@@ -12,6 +13,8 @@ if __name__ == '__main__':
         f.write('\n\n##')
         f.write(_homepage)
         f.write('\n\n##')
-        f.write(_news)
+        # f.write(_news)
         f.write('\n\n##')
         f.write(_pub)
+        f.write('\n\n')
+        f.write(_starts)
